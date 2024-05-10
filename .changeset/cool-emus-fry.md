42cb223aa5ae8cb67aefe51e6d0dae95588e0552
nurliman <nurlimandiara@gmail.com>
2024-05-10 11:47:52 +0700

    fix: add return type to charCodeArrayToString function

---
+++ .changeset/cool-emus-fry.md
@@ -0,0 +1,5 @@
+---
+"@nurliman/base85": patch
+---
+
+add return type to charCodeArrayToString function
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export function charCodeArrayToString(charCodes: number[]) {
+export function charCodeArrayToString(charCodes: number[]): string {
   let output = "";
   for (let i = 0; i < charCodes.length; i++) {
     output += String.fromCharCode(charCodes[i]);
